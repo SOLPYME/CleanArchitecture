15f41274fd94cb5c1a09f2c8e665a7651941f133
Juan Manuel Pardo García <solpyme@outlook.es>
2022-06-27 23:25:43 +0200

    Insert data into the Database for Streamer and Videos.

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+StreamerDbContext _dbContext = new();
+await QueryStreaming();
+
+async Task QueryStreaming()
+{
+    Streamer _streamer = new()
+    {
+        Name = "Amazon Prime",
+        Url = "http:\\www.amazonprime.com"
+    };
+
+    _dbContext.Streamers!.Add(_streamer);
+
+    await _dbContext.SaveChangesAsync();
+
+    var _movies = new List<Video>
+    {
+        new Video
+        {
+            Name = "Mad Max",
+            StreamerId = _streamer.Id
+        },
+        new Video
+        {
+            Name = "Batman",
+            StreamerId = _streamer.Id
+        },
+        new Video
+        {
+            Name = "Crepusculo",
+            StreamerId = _streamer.Id
+        },
+        new Video
+        {
+            Name = "Citizen Kane",
+            StreamerId = _streamer.Id
+        }
+    };
+
+    await _dbContext.Videos!.AddRangeAsync(_movies);
+    await _dbContext.SaveChangesAsync();
+}
