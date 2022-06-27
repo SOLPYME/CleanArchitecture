6032fa0ecf219c5c6a51b25ac617f8948b7c7df1
Juan Manuel Pardo García <solpyme@outlook.es>
2022-06-27 21:23:21 +0200

    Define the Context Class and the Database. We will use a connection string for a SQL Server Express

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"DataSource=localhost\sqlexpress; Initial Catalog=Streamer; Integrated Security=True");
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+
+
+    }
+}
