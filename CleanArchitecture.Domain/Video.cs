927743869ed1e76f8b7d84086fcd69152cb64e8a
Juan Manuel Pardo García <solpyme@outlook.es>
2022-06-27 21:13:49 +0200

    Define the Class of the Data Model for Video

--- CleanArchitecture.Domain/Video.cs
+++ CleanArchitecture.Domain/Video.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanArchitecture.Domain
+﻿namespace CleanArchitecture.Domain
 {
     public class Video
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int StreamerId { get; set; }
+        public virtual Streamer? Streamer { get; set; }
     }
 }
