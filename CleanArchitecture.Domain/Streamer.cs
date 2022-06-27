5516fa614e8847f431c4c8fc62f767079b22501d
Juan Manuel Pardo García <solpyme@outlook.es>
2022-06-27 21:15:09 +0200

    Define the Class of the Data Model for Streamer

--- CleanArchitecture.Domain/Streamer.cs
+++ CleanArchitecture.Domain/Streamer.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanArchitecture.Domain
+﻿namespace CleanArchitecture.Domain
 {
     public class Streamer
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
     }
 }
